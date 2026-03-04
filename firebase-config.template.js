081bf9a55386ccba3522971af27e23a67e045bd0
wjymonica <wangchy@umich.edu>
2026-03-03 23:54:11 -0500

    move OpenAI key to env var, gitignore firebase-config.js

---
+++ firebase-config.template.js
@@ -0,0 +1,19 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBWc3GcWlbLbEt8wSLbAE2nuPRwph7pCT8",
+  authDomain: "surgqtest.firebaseapp.com",
+  databaseURL: "https://surgqtest-default-rtdb.firebaseio.com",
+  projectId: "surgqtest",
+  storageBucket: "surgqtest.firebasestorage.app",
+  messagingSenderId: "871045470522",
+  appId: "1:871045470522:web:2d51714501422bec8bd803",
+  measurementId: "G-BCZKTYJVL9"
+};
+
+window.APP_CONFIG = {
+  mode: "v1",
+};
+
+window.OPENAI_CONFIG = {
+  apiKey: "__OPENAI_API_KEY__",
+  model: "gpt-4o-mini",
+};
